d4c1710e0f572980910c7466dc66ea5cfc0a718d
Gabriel <Gabrielkhalfin@gmail.com>
2021-01-09 19:53:52 -0500

    Assignment 4 Final

---
+++ js/script.js
@@ -0,0 +1,71 @@
+
+// Question 1
+function Button1() {
+	let clickButton1 = document.getElementById('container');
+	let button1Text = document.createElement('section');
+	button1Text.innerText = "I'm right";
+	clickButton1.appendChild(button1Text);
+}
+
+function Button2() {
+	let clickButton2 = document.getElementById('container');
+	let button2Text = document.createElement('section');
+	button2Text.innerHTML = "No, I'm right!";
+	clickButton2.appendChild(button2Text);
+}
+
+
+
+// Question 2
+function onHover() {
+    let change = document.createElement('section');
+	change.innerHTML = alert("Hey, I told you not to hover over me!");
+}
+
+//Once loaded in
+window.onload = function() {
+    document.querySelector('#button1').addEventListener('click', Button1);
+    document.querySelector('#button2').addEventListener('click', Button2);
+    document.querySelector('#hover').addEventListener('mouseover', onHover);
+  };
+
+
+  
+
+// Question 3 and 4
+function clickingOnTheSubmitButton(event) {
+	event.preventDefault();
+	let belowTheSubmitButton = document.getElementById('q5');
+	let username = document.getElementById('input1').value;
+	let password = document.getElementById('input2').value;
+	let addingAH1 = document.createElement('h1');
+	belowTheSubmitButton.appendChild(addingAH1);
+
+	for (var i = 0; i <= username.length; i++) {
+			if (parseFloat(username.length) && parseInt(password) === 12345678){
+			addingAH1.innerHTML = 'Correct';
+		} else if(username.length == ""){
+            addingAH1.innerHTML = 'Please enter Username';
+
+        }else {
+			addingAH1.innerHTML = 'Incorrect';
+        }
+        
+	}
+}
+
+//Extra Credit
+function calculateVolume(){
+    let radius = document.getElementById("radius").value;
+    if(radius >= 0){
+        let volume = (4*Math.PI*radius*radius*radius)/3;
+        volume = volume.toFixed(2);
+        document.getElementById("volume-answer").innerHTML = volume;
+    }
+    else{
+        alert("Radius cannot be negative!");
+    }
+    return false;
+}
+
+
